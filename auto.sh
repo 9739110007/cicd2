c6677c372bbb5f285d681e3f790b16e77830f7e9
DIVYA <142962877+Divya7508@users.noreply.github.com>
2024-02-05 16:27:28 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -18,3 +18,25 @@ docker push container548/hello:"$tag1"
 
 # Update deployment.yml with the new tag
 sed -i "s/latest/$tag1/g" deployment.yml
+
+#########################################################################################################
+
+#sed -n '7s/.*<version>\(.*\)<\/version>/\1/p' pom.xml:
+# explaination of command
+
+#sed :is the stream editor, used for text transformations.
+#-n :option suppresses automatic printing of pattern space.
+##7s/.*<version>\(.*\)<\/version>/\1/p: This sed command looks for the 7th line in the file pom.xml, matches the pattern between <version> and </version>, and prints only that matched part.
+# .*<version>\(.*\)<\/version>: This is the regular expression to match the entire line containing <version> tags, and it captures the content between the tags.
+#\1: In the replacement part, it refers to the content captured within the parentheses.
+#p: Prints the result.
+# | awk -F '-' '{print $1}':| is the pipe operator, which takes the output of the preceding command and passes it as input to the following command.
+#awk is a text processing tool that operates on fields within a line.
+#-F '-': Specifies that the field separator is the hyphen ("-").
+#{print $1}: Prints the first field (the part before the hyphen).
+ # So, in summary, this command extracts the version information from the 7th line of the pom.xml file, then uses awk to extract the part before the hyphen in case there is a hyphenated version (common in version strings like "1.0-SNAPSHOT"). The result is stored in the variable tag1.
+
+
+
+
+
