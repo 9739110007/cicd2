39e1bc6e27f2746b2e61c02cddc72b7a64f73a96
DIVYA <142962877+Divya7508@users.noreply.github.com>
2024-02-05 16:28:12 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,21 +1,10 @@
-
 #!/bin/bash
-
 # Extracting version from pom.xml
 tag1=$(sed -n '7s/.*<version>\(.*\)<\/version>/\1/p' pom.xml | awk -F '-' '{print $1}')
-
-# Display Docker version
 docker version
-
-# Build Docker image
 docker build -t hello .
-
-# Tag Docker image
 docker tag hello container548/hello:"$tag1"
-
-# Push Docker image to registry
 docker push container548/hello:"$tag1"
-
 # Update deployment.yml with the new tag
 sed -i "s/latest/$tag1/g" deployment.yml
 
